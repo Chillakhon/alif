ff946ddd4ebdb1963514f4c71bb079be3c492d90
chollakhon <shoev-03@mail.ru>
2023-07-18 17:04:59 +0500

    update

--- task.php
+++ task.php
@@ -3,5 +3,41 @@ $fileName = $argv[1]; // Название файла
 $action = $argv[2]; // Действие
 $line = $argv[3]; // Строка, которую записываем в файл
 
+class File {
+    private string $fileName;
 
+    public function __construct(string $fileName, string $action)
+    {
+        $this->fileName = $fileName;
+        if(!self::fileCheck($this->fileName)) {
+            exit('Файл не найден');
+        }
+
+        if(!method_exists($this, $action)) {
+            exit('Действие не найдено');
+        }
+    }
+
+    private static function fileCheck(string $fileName) : bool
+    {
+        return file_exists($fileName);
+    }
+    protected function getOpenFileForWriting()
+    {
+        $fileForWriting =  fopen($this->fileName, 'a') or die("Не удалось открыть файл");
+        return $fileForWriting;
+    }
+
+    public function addLine(string $line)
+    {
+        $openFileForWriting = $this->getOpenFileForWriting();
+        fwrite($openFileForWriting, $line . PHP_EOL);
+        fclose($openFileForWriting);
+        echo 'Строка добавлена';
+    }
+}
+
+
+$objFile = new File(fileName: $fileName, action: $action);
+$objFile->$action($line);
 
