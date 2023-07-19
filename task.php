98ae5c1c8f590b8ba47098d2036411eb829f722c
chollakhon <shoev-03@mail.ru>
2023-07-19 11:06:14 +0500

    update

--- task.php
+++ task.php
@@ -1,46 +1,79 @@
 <?php
-$fileName = $argv[1]; // Название файла
-$action = $argv[2]; // Действие
-$line = $argv[3]; // Строка, которую записываем в файл
 
-class File {
+class Products
+{
     private string $fileName;
 
     public function __construct(string $fileName, string $action)
     {
         $this->fileName = $fileName;
-        if(!self::fileCheck($this->fileName)) {
+        if (!self::fileCheck($this->fileName)) {
             exit('Файл не найден');
         }
 
-        if(!method_exists($this, $action)) {
+        if (!method_exists($this, $action)) {
             exit('Действие не найдено');
         }
     }
 
-    private static function fileCheck(string $fileName) : bool
+    private static function fileCheck(string $fileName): bool
     {
         return file_exists($fileName);
     }
+
     protected function getOpenFileForWriting()
     {
-        $fileForWriting =  fopen($this->fileName, 'a') or die("Не удалось открыть файл");
+        $fileForWriting = fopen($this->fileName, 'a') or die("Не удалось открыть файл");
         return $fileForWriting;
     }
 
-    public function add(string $line)
+    private function getProductsFromFile(): array
+    {
+        return array_filter(file($this->fileName)); // Получаем все записи из файла в виде массива
+    }
+
+    public function addLine(string $line)
     {
         $openFileForWriting = $this->getOpenFileForWriting();
         fwrite($openFileForWriting, $line . PHP_EOL);
         fclose($openFileForWriting);
         echo 'Строка добавлена';
     }
+
+    public function edit($productName, $newPrice)
+    {
+        $products = $this->getProductsFromFile();// Получаем все записи из файла в виде массива
+
+        foreach ($products as $key => $product) {
+            $product = explode('-', $product);
+
+            $productNameForFile = $product[0]; //Получаем название продукта из файла
+
+            if ($productNameForFile == $productName) {
+                $productPrice = $newPrice;
+                $product = $productName . '-' . $newPrice;
+                $products[$key] = $product . PHP_EOL;
+                file_put_contents($this->fileName, $products);
+                exit('Цена изменена');
+            } else {
+                exit('Переданный продукт не найден.');
+            }
+        }
+    }
+}
+
+$fileName = $argv[1]; // Название файла
+$action = $argv[2] ?? null; // Действие
+$data = $argv[3] ?? null; // Данные, которую записываем в файл
+$newPrice = $argv[4] ?? null; //Новая цена которую будем устанавливать для продукта
+
+$objFile = new Products(fileName: $fileName, action: $action);
+
+if ($action == 'edit') {
+    $objFile->$action($data, $newPrice);
+} else {
+    (!empty($data)) ? $objFile->$action($data) : $objFile->$action();
 }
 
 
-$objFile = new File(fileName: $fileName, action: $action);
-$objFile->$action($line);
-
-$lines = explode("\n",file_get_contents("products.txt"));
-var_dump($lines);
 
